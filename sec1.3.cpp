b2ce5ffc53b4bccbb24e60b9cf597f2452fcc13d
flipmcf <flipmcf@gmail.com>
2015-02-07 12:11:31 -0500

    ex 1.7

---
+++ sec1.3.cpp
@@ -0,0 +1,13 @@
+// exercise 1.7: compile a program that has incorrecctly nested comments
+
+int main()
+{
+/* this is a comment
+  /* that is incorrectly nested */
+  and this line is an error  */
+  
+  return 0;
+
+/* fyi - my IDE also hates me now */
+
+}
