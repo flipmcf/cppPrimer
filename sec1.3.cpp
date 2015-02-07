a64faf82ac70c5bfec4d9c462b46bd90952742d7
flipmcf <flipmcf@gmail.com>
2015-02-07 12:14:15 -0500

    yep - that's an error

--- sec1.3.cpp
+++ sec1.3.cpp
@@ -11,3 +11,11 @@ int main()
 /* fyi - my IDE also hates me now */
 
 }
+
+
+// flip@GazelleMcF:~/c++primer$ c++ -o sec1.3 sec1.3.cpp
+// sec1.3.cpp: In function ‘int main()’:
+// sec1.3.cpp:7:7: error: expected identifier before ‘this’
+//   and this line is an error  */
+//       ^
+// sec1.3.cpp:7:7: error: expected ‘;’ before ‘this’
