58611ca0ce7759a1284c2f93e5736adb2304721f
flipmcf <flipmcf@gmail.com>
2015-02-07 11:32:22 -0500

    1.4

--- ex1.3.cpp
+++ ex1.3.cpp
@@ -2,6 +2,13 @@
 
 int main()
 {
-    std::cout << "Hello World" << std::endl;
+    int x = 0, y = 0;
+
+    std::cout << "Gimme two numbers to multuply" << std::endl;
+    
+    std::cin >> x >> y;
+    
+    std::cout << "the product of " << x << " and " << y 
+              << " is " << x * y << std::endl;
     return 0;
 }
