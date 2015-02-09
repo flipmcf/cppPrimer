95820a1067d1cb46b0b6a6c385d7e88f9e785eef
flipmcf <flipmcf@gmail.com>
2015-02-08 20:18:57 -0500

    add comments and apologize

--- sec1.4.3.cpp
+++ sec1.4.3.cpp
@@ -4,7 +4,12 @@ int main()
 {
     int value = 0, sum = 0;
     
+    //This will read in values from stdin and sum them, until EOF is reached:
     for(; std::cin >> value; sum += value){}
+        
+    //However, this is unreadable and relies on side effects.        
+    //I would never write this kind of code in normal circumstances.
+    // I feel so dirty inside right now I have to write these comments.
         
     std::cout << "total is " << sum << std::endl;
     
