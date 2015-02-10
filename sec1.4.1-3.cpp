8e23b20c63ed9fad9fcd058ec434cf5241f1b2b3
flipmcf <flipmcf@gmail.com>
2015-02-10 10:40:57 -0500

    exercise 1.19

--- sec1.4.1-3.cpp
+++ sec1.4.1-3.cpp
@@ -6,6 +6,13 @@ int main()
     std::cout << "enter two numbers - and I'll give you a range: " << std::endl;
     std::cin >> low >> high;
     
+    if (low > high){
+        int temp;
+        temp = high;
+        high = low;
+        low = temp;
+    }    
+    
     int x = low;
     while (x <= high)
     {
