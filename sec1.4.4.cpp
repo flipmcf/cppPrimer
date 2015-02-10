92777a74a6bfa30c010f5623b9ecb0bdfa0e038c
flipmcf <flipmcf@gmail.com>
2015-02-09 21:19:13 -0500

    exercise 1.17 & 1.18

---
+++ sec1.4.4.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+
+int main()
+{
+    // currVal is the number we're counting; we'll read new values into val
+    int currVal = 0, val = 0;
+    
+    //  read first number and ensure that we have data to process
+    if (std::cin >> currVal) {
+        int cnt = 1;
+        while (std::cin >> val){
+            if (val == currVal)
+                ++cnt;
+            else {
+                std::cout << currVal << " occurs "
+                          << cnt << " times" << std::endl;
+                currVal = val;
+                cnt = 1;
+            }
+        } // end while
+        // remember to print out the last value
+        std::cout << currVal << " occurs "
+                  << cnt << " times" << std::endl;
+            
+    } // end if
+    return 0;
+}
